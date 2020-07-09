5a634bcdabb258f73bc4c81e368d11475f04e54f
Olga <okozmovskaya@icloud.com>
2020-07-09 22:13:51 +0200

    Completed exercise 1(c)

--- exercises/1-reverse-string/index.js
+++ exercises/1-reverse-string/index.js
@@ -1,7 +1,15 @@
 /**
+ * A function that returns reversed string.
+ *
+ * @param {str}  - string for reversed
+ * 
+ * @example
+ * reverseString('hello there') // returns 'ereht olleh'
  *
  */
-const reverseString = () => {
+const reverseString = (str) => {
+   
+    return str.split("").reverse().join("");
 
 }
 
