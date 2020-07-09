e8fd8e79b1a0cb270aa25da49ed79768fdd3dcc1
Olga <okozmovskaya@icloud.com>
2020-07-09 18:52:41 +0200

    Completed exercise 1(a)

--- exercises/1-remove-from-array/index.js
+++ exercises/1-remove-from-array/index.js
@@ -1,8 +1,24 @@
 /**
+ * A function that takes an array and some other arguments then removes the other arguments from that array.
+ *
+ * @param {arr}  - array who returns with remove item(s)
+ * @returns {args} - arguments for remove from array
+ *
+ * @example
+ * removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
  *
  */
-const removeFromArray = () => {
+const removeFromArray = (arr, ...args) => {
+    
+    for (let a = 0; a < args.length; a++) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === args[a]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
 
+    return arr;
 };
 
 module.exports = removeFromArray
--- exercises/1-repeat-string/index.js
+++ exercises/1-repeat-string/index.js
@@ -1,7 +1,14 @@
 /**
  *
  */
-const repeatString = () => {
+const repeatString = (arr, ... args) => {
+    for (i = 0, i < arr, i++) {
+        for (a = 0, a < args, a++) {
+            if (i === a) {
+                arr.splice(i, 1)
+            }
+        }
+    }
 
 }
 
