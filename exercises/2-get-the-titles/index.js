79a2acc4045be27458aa554de2da816fd1af34a8
Olga <okozmovskaya@icloud.com>
2020-07-22 18:03:04 +0200

    Completed 2-get-the-titles

--- exercises/2-get-the-titles/index.js
+++ exercises/2-get-the-titles/index.js
@@ -1,7 +1,25 @@
-/**
- *
+/** There is function `getTheTitles` which returns required item from object into array.
+ * @param {array} - array with objects
+ * @param {object} - object into array with several items
+ * @returns array with required items from all objects into array
+ * @example
+ * 
+ * const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+    getTheTitles(books) // ['Book','Book2']
  */
-const getTheTitles = () => {
+const getTheTitles = (books) => {
+    let showTitle = books.map(item => item.title);
+    return showTitle;
 
 }
 
