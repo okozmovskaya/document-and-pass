00deb7b6a5346acffdbc786f222ace2a422732dd
Olga <okozmovskaya@icloud.com>
2020-07-22 16:58:57 +0200

    Completed sum-all

--- exercises/1-sum-all/index.js
+++ exercises/1-sum-all/index.js
@@ -1,7 +1,23 @@
 /**
- *
+ * The function `sumAll` have two parameters (numbers) that are the start and the end of array. The function returns the sum all numbers in array. 
+ * @param {number} - start of array
+ * @param {number} - end of array
+ * @returns {number} - sum all numbers in array
  */
-const sumAll = () => {
+const sumAll = (start, end) => {
+    let sum = 0;
+
+    if (start <= 0 || end <= 0 || typeof end !== 'number' || typeof start !== 'number') {
+        return sum = 'ERROR';
+    } else {
+        let firstTerm = start > end ? end : start;
+        let lastTerm = end < start ? start : end;
+    
+        for (let i = firstTerm; i <= lastTerm; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 
 }
 
