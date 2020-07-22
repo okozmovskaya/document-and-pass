35130f9cabf5f1cfebcbb98ef9e95278a043788b
Olga <okozmovskaya@icloud.com>
2020-07-22 17:31:05 +0200

    Completed 2-fibonacci

--- exercises/2-fibonacci/index.js
+++ exercises/2-fibonacci/index.js
@@ -1,8 +1,19 @@
-/**
- *
+/** There is the function Fibonacci which have one parameter number - index number of Fibonacci series. The function returns also number under the given index.
+ *@param {number} - index number of Fibonacci series
+  @returns {number} - number under index of Fibonacci series
  */
-const fibonacci = () => {
-
+const fibonacci = (n) => {
+        let arr = [0, 1];
+        if (n < 0) {
+            return arr = "OOPS";
+        } else {
+            for (let i = 2; i < n + 1; i++){
+                arr.push(arr[i - 2] + arr[i -1])
+              }
+        }  
+        
+       return arr[n];
+      
 }
 
 module.exports = fibonacci
