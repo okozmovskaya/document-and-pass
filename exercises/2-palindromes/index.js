e70ba88bece45d0f8aba98f5bbf651cf26fea0e2
Olga <okozmovskaya@icloud.com>
2020-07-23 15:23:20 +0200

    Completed 3-caesar

--- exercises/2-palindromes/index.js
+++ exercises/2-palindromes/index.js
@@ -1,8 +1,25 @@
-/**
- *
- */
-const palindromes = () => {
+/** This function determines whether or not a given string is a palindrome.
+ *@param {str} - given string for palindrome
+ @return {boolean} - true if the string is palindrome and false on the contrary
+ 
+ @example 
 
+palindromes('racecar') // true
+palindromes('tacos') // false
+
+ */
+
+const palindromes = (str) => {
+    let justCharacter = /[\W_]/g;
+    let lowerCaseStr = str.toLowerCase().replace(justCharacter, '');
+    let reverseStr = lowerCaseStr.split('').reverse().join(''); 
+
+    if (lowerCaseStr === reverseStr) {
+        return true;
+
+    } else {
+        return false;
+    }
 };
 
 module.exports = palindromes;
--- exercises/3-caesar/index.js
+++ exercises/3-caesar/index.js
@@ -1,8 +1,32 @@
-/**
- *
+/** This function takes a string to be encoded and a shift factor and then returns the encoded string in Caesar cipher system.
+ *@param {str} - string to be encoded
+ @param {number} - shift factor
+ @return {str} - encoded Caesar string
+ @example:
+ caesar('Hey', 5) // returns 'Mjd;
  */
-const caesar = () => {
+const caesar = (str, keyShift) => { 
+    if (keyShift < 0) {
+        return caesar (str, keyShift + 26);
+    }
+    
+    let finishStr = '';
 
+    for (i = 0; i < str.length; i++) {
+        let letter = str[i];
+        
+        if (letter.match(/[a-z]/i)) {
+            let codeUnicode = str.charCodeAt(i);
+
+            if (codeUnicode >= 65 && codeUnicode <= 90) {
+                letter = String.fromCharCode(((codeUnicode - 65 + keyShift) % 26) + 65);
+            } else if (codeUnicode >= 97 && codeUnicode <= 122) {
+                letter = String.fromCharCode(((codeUnicode - 97 + keyShift) % 26) + 97);
+            }
+        };
+        finishStr += letter;
+    }
+    return finishStr;
 };
 
 module.exports = caesar;
